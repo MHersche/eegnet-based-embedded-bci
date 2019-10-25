29f3e1a5d43d6097d51c17f99af28120f86783fb
Burak Alp Kaya <bukaya@student.ethz.ch>
2019-10-25 20:13:08 +0200

    Fixed Comments, Rearranged some code. RENAME

--- data_tester.py
+++ data_tester.py
@@ -6,6 +6,12 @@ __email__ = "tbatuhan@ethz.ch, bukaya@ethz.ch"
     Program to test given data. Looks for variations in the signal properties and reports them back to the user.
     
     USAGE: tune file_name line to look for correct data locations, change default values for signal parameters
+    example usage:
+        PATH = "../files/"
+        s = [i for i in range(1,110)]
+        r = [i for i in range(3,15)]
+        subs = test_data(s, r, PATH, 160, 64, 30)
+
 """
 
 # pyEDFlib is a python library to read/write EDF+/BDF+ files based on EDFlib.
@@ -13,24 +19,24 @@ import pyedflib
 import numpy as np
 import os
 
-def test_data(subjects, runs, PATH):
+def test_data(subjects, runs, PATH, default_frequency, default_no_channels, default_no_events):
     
     '''
     Keyword arguments:
-    subject -- array of subject numbers in range [1, .. , 109] (integer)
-    runs    -- array of the numbers of the runs in range [1, .. , 14] (integer)
-    PATH    -- string of PATH to directory containing .edf data
-               ex: /home/<username>/<project_directory>/<data>/
-               to this, SXXXRYY.edf will be added to form the full path
-    
-    Output: Prints variations in data
-        
-    Return: PATHS -- PATHs to irregular data in a String List
+    subjects -- array of subject numbers in range [1, .. , 109] (integer)
+    runs     -- array of the numbers of the runs in range [1, .. , 14] (integer)
+    PATH     -- string of PATH to directory containing .edf data
+                ex: /home/<username>/<project_directory>/<data>/
+                to this, SXXXRYY.edf will be added to form the full path
+
+    Return:
+    subjects -- array of subjects who were not removed
     '''
     # Define expected values
-    frequency = 160
-    eeg_channels = 64
-    events = 30
+    frequency = default_frequency       #160 [Hz]
+    eeg_channels = default_no_channels  #60
+    events = default_no_events          #30
+    
     for subject in subjects:
         for run in runs:
             
@@ -48,26 +54,27 @@ def test_data(subjects, runs, PATH):
     
             # Create file name variable to access edf file
             file_name = os.path.join(PATH +'S' + subject_str,'S'+ subject_str + 'R' + run_str + '.edf')
-                
+            
             # Read file
             f = pyedflib.EdfReader(file_name)
                 
-            # Signal Parameters - measurement frequency
-            freq = f.getSampleFrequencies()
-            for i in np.arange(1, len(freq)):
-                if (freq[i-1] != freq[i]):
-                    print('S'+ subject_str + 'R' + run_str + " contains channels with differing frequencies!")
-            
+            # Signal Parameters
             # Number of eeg channels = number of signals in file
             n = f.signals_in_file
-    
             # Get Label information
             annotations = f.readAnnotations()
             labels = annotations[2]
-            
+            # Measurement Frequency
+            freq = f.getSampleFrequencies()
+                        
             # Check irregularities
+            for i in np.arange(1, len(freq)):
+                if (freq[i-1] != freq[i]):
+                    print('S'+ subject_str + 'R' + run_str + " contains channels with differing frequencies!")
+                    if subject in subjects: subjects.remove(subject)            
+            
             if (frequency != freq[0]):
-                print('S'+ subject_str + 'R' + run_str + "  Frequency different: " + str(frequency) + " to: " + str(freq[0]))
+                print('S'+ subject_str + 'R' + run_str + "  removed: Frequency different: " + str(frequency) + " to: " + str(freq[0]))
                 if subject in subjects: subjects.remove(subject)
             
             if (eeg_channels != n):
@@ -77,15 +84,6 @@ def test_data(subjects, runs, PATH):
             if (events != len(labels)):
                 print('S'+ subject_str + 'R' + run_str + "  Number of Events per Run different: " + str(events) + " to: " + str(len(labels)))
                 if subject in subjects: subjects.remove(subject)
-        
+                
             f.close()
     return subjects
-
-
-#""" example usage:
-    
-PATH = "/home/bukaya/mlEEG/files/"
-s = [i for i in range(1,110)]
-r = [i for i in range(3,15)]
-subs = test_data(s, r, PATH)
-#""" 
