ec5e7792caf77e40d8fd29441319a1b09921bd89
Burak Alp Kaya <bukaya@student.ethz.ch>
2019-11-06 23:31:13 +0100

    Fixed data getting part

--- main_phy_kfold_all.py
+++ main_phy_kfold_all.py
@@ -8,8 +8,8 @@ import os
 # pyEDFlib is a python library to read/write EDF+/BDF+ files based on EDFlib.
 import pyedflib
 # our functions to test and load data
-import get_data_edf_4class as get
-import data_tester as tst
+import get_4class as get
+#import data_tester as test
 # tensorflow part
 from tensorflow.keras import utils as np_utils
 from tensorflow.keras.callbacks import ModelCheckpoint
@@ -23,61 +23,17 @@ from sklearn.model_selection import StratifiedKFold
 
 # Set data parameters
 PATH = "../files/"
-subjects = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20
-            ,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38
-            ,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54
-            ,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71
-            ,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,89
-            ,90,91,93,94,95,96,97,98,99,101,102,103,105,107,108,109]
 
-subjects2 = [1,2,3,4,5,6,7,8,9,10]
-
-training_runs = [4,6,8,10]
-test_runs = [12,14]
-
-# Test data: uncomment and run to reproduce above initializes subjects array
-#subjects = [i for i in range(1,110)]
-#subjects = tst.test_data(subjects, runs, PATH)
-
-# Load only consistent data
-X_Train, y_Train = get.get_data(subjects2, training_runs, PATH)
-
-X_Test, y_Test = get.get_data(subjects2, test_runs, PATH)
-
-"""
-# Test shuffling - seems to work - delete after making sure
-X_Train_cp = X_Train.copy()
-y_Train_cp = y_Train.copy()
-X_Test_cp = X_Test.copy()
-y_Test_cp = y_Test.copy()
-tr = [i for i in range(1,54)]
-tr_cp = [i for i in range(1,54)]
-np.random.seed(42)
-np.random.shuffle(tr)
-"""
-
-# Suffle data reproducably
-np.random.seed(42)
-np.random.shuffle(X_Train)
-np.random.seed(42)
-np.random.shuffle(y_Train)
-np.random.seed(42)
-np.random.shuffle(X_Test)
-np.random.seed(42)
-np.random.shuffle(y_Test)
-
+# Load data
+X_Train, y_Train = get.get_data(PATH)
+# Expand dimensions to match expected EEGNet input
 X_Train_real = (np.expand_dims(X_Train, axis=1))
-X_Test_real  = (np.expand_dims(X_Test, axis=1))
 
 # use sample size
 SAMPLE_SIZE = np.shape(X_Train_real)[3]
 
 # convert labels to one-hot encodings.
 y_Train_cat      = np_utils.to_categorical(y_Train)
-y_Test_cat       = np_utils.to_categorical(y_Test)
-
-# TODO: implement k-fold cross validation on prepared dataset
-# TODO: calculate/determine EEGNet parameters
 
 # using 5 folds
 kf = StratifiedKFold(n_splits = 5)
