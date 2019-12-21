3ba000137652dfd755d21aa12d4eba9f5f07a7f6
Burak Alp Kaya <bukaya@student.ethz.ch>
2019-12-21 15:30:00 +0100

    Finished subject retrainer before bug testint

--- main_subject_retrainer.py
+++ main_subject_retrainer.py
@@ -40,7 +40,7 @@ def exclude_subjects(all_subjects=range(1,110), excluded_subjects=[88,92,100,104
 #################################################
 
 # Number of epochs to use with 10^[-3,-4,-5] Learning Rate
-epochs = [20,30,50]
+epochs = [2,3,5]
 lrates = [-3,-4,-5]
 
 
@@ -48,7 +48,7 @@ lrates = [-3,-4,-5]
 PATH = "../files/"
 
 current_time = datetime.now()
-results_dir=f'global_models'
+results_dir=f'subject_specific'
 #os.makedirs(results_dir, exist_ok=True)
 os.makedirs(f'{results_dir}/stats', exist_ok=True)
 os.makedirs(f'{results_dir}/model', exist_ok=True)
@@ -71,7 +71,7 @@ for num_classes in num_classes_list:
     # using 5 folds
     num_splits = 5
     kf_global = KFold(n_splits = num_splits)
-    n_epochs = 100
+    n_epochs = 10
 
     split_ctr = 0
     for train_global, test_global in kf_global.split(X_Train_real, y_Train):
@@ -87,88 +87,96 @@ for num_classes in num_classes_list:
             for train_sub, test_sub in kf_subject.split(X_sub, y_sub):
                 print(f'N_Classes:{num_classes}, Model: {split_ctr} \n Subject: {subject:03d}, Split: {sub_split_ctr}')
                 model = load_model(f'global_models/model/global_class_{num_classes}_split_{split_ctr}_v1.h5')
+                first_eval = model.evaluate(X_sub, y_sub_cat, batch_size=16) 
                 train_accu = np.array([])
                 valid_accu = np.array([])
                 train_loss = np.array([])
                 valid_loss = np.array([])
-                model = models.EEGNet(nb_classes = num_classes, Chans=64, Samples=SAMPLE_SIZE, regRate=0.25,
-                                dropoutRate=0.2, kernLength=128, numFilters=8, dropoutType='Dropout')
+                # The first elements of the arrays from evaluation
+                train_accu = np.append(train_accu, first_eval[1])
+                valid_accu = np.append(valid_accu, first_eval[1])
+                train_loss = np.append(train_loss, first_eval[0])
+                valid_loss = np.append(valid_loss, first_eval[0])
                 for j, lrate in enumerate(lrates):
                     # Set Learning Rate
                     adam_alpha = Adam(lr=(10**lrate))
                     model.compile(loss='categorical_crossentropy', optimizer=adam_alpha, metrics = ['accuracy'])
                     # creating a history object
-                    history = model.fit(X_Train_real[train_global], y_Train_cat[train_global], 
-                            validation_data=(X_Train_real[test_global], y_Train_cat[test_global]),
+                    history = model.fit(X_sub[train_sub], y_sub_cat[train_sub], 
+                            validation_data=(X_sub[test_sub], y_sub_cat[test_sub]),
                             batch_size = 16, epochs = epochs[j], verbose = 2)
                     train_accu = np.append(train_accu, history.history['acc'])
                     valid_accu = np.append(valid_accu, history.history['val_acc'])
                     train_loss = np.append(train_loss, history.history['loss'])
                     valid_loss = np.append(valid_loss, history.history['val_loss'])
-
+                sub_str = '{0:03d}'.format(subject)
                 # Save metrics
-                train_accu_str = f'{results_dir}/stats/train_accu_split_{split_ctr}_v1.csv'
-                valid_accu_str = f'{results_dir}/stats/valid_accu_split_{split_ctr}_v1.csv'
-                train_loss_str = f'{results_dir}/stats/train_loss_split_{split_ctr}_v1.csv'
-                valid_loss_str = f'{results_dir}/stats/valid_loss_split_{split_ctr}_v1.csv'
-             
-        np.savetxt(train_accu_str, train_accu)
-        np.savetxt(valid_accu_str, valid_accu)
-        np.savetxt(train_loss_str, train_loss)
-        np.savetxt(valid_loss_str, valid_loss)
+                train_accu_str = f'{results_dir}/stats/train_accu_v1_class_{num_classes}_subject_{sub_str}_fold_{sub_split_ctr}.csv'
+                valid_accu_str = f'{results_dir}/stats/valid_accu_v1_class_{num_classes}_subject_{sub_str}_fold_{sub_split_ctr}.csv'
+                train_loss_str = f'{results_dir}/stats/train_loss_v1_class_{num_classes}_subject_{sub_str}_fold_{sub_split_ctr}.csv'
+                valid_loss_str = f'{results_dir}/stats/valid_loss_v1_class_{num_classes}_subject_{sub_str}_fold_{sub_split_ctr}.csv'
+                     
+                np.savetxt(train_accu_str, train_accu)
+                np.savetxt(valid_accu_str, valid_accu)
+                np.savetxt(train_loss_str, train_loss)
+                np.savetxt(valid_loss_str, valid_loss)
 
-        #Save model
-        print('Saving model...')
-        model.save(f'{results_dir}/model/global_class_{num_classes}_split_{split_ctr}_v1.h5')
+                #Save model
+                #print('Saving model...')
+                #model.save(f'{results_dir}/model/subspec_v1_class_{num_classes}_subject_{sub_str}_fold_{sub_split_ctr}.h5')
 
-        #Clear Models
-        K.clear_session()
+                #Clear Models
+                K.clear_session()
         split_ctr = split_ctr + 1
-
 for num_classes in num_classes_list:
-    # Once all CV folds are done, calculate averages, plot, and save
-    train_accu = np.zeros(n_epochs)
-    valid_accu = np.zeros(n_epochs)
-    train_loss = np.zeros(n_epochs)
-    valid_loss = np.zeros(n_epochs)
-    
-    for split_ctr in range(0,5)
-        train_accu_step = np.loadtxt(f'{results_dir}/stats/train_accu_split_{split_ctr}_v1.csv')
-        valid_accu_step = np.loadtxt(f'{results_dir}/stats/valid_accu_split_{split_ctr}_v1.csv')
-        train_loss_step = np.loadtxt(f'{results_dir}/stats/train_loss_split_{split_ctr}_v1.csv')
-        valid_loss_step = np.loadtxt(f'{results_dir}/stats/valid_loss_split_{split_ctr}_v1.csv')
-        train_accu += train_accu_step
-        valid_accu += valid_accu_step
-        train_loss += train_loss_step
-        valid_loss += valid_loss_step
-    
-    train_accu = train_accu/num_splits
-    valid_accu = valid_accu/num_splits
-    train_loss = train_loss/num_splits
-    valid_loss = valid_loss/num_splits
+    split_ctr = 0
+    for train_global, test_global in kf_global.split(X_Train_real, y_Train):
+        for sub_idx in test_global:
+            subject = subjects[sub_idx]
+            train_accu = np.zeros(n_epochs+1)
+            valid_accu = np.zeros(n_epochs+1)
+            train_loss = np.zeros(n_epochs+1)
+            valid_loss = np.zeros(n_epochs+1)
+            sub_str = '{0:03d}'.format(subject)
+            for sub_split_ctr in range(0,4):
+                # Save metrics
+                train_accu_step = np.loadtxt(f'{results_dir}/stats/train_accu_v1_class_{num_classes}_subject_{sub_str}_fold_{sub_split_ctr}.csv')
+                valid_accu_step = np.loadtxt(f'{results_dir}/stats/valid_accu_v1_class_{num_classes}_subject_{sub_str}_fold_{sub_split_ctr}.csv')
+                train_loss_step = np.loadtxt(f'{results_dir}/stats/train_loss_v1_class_{num_classes}_subject_{sub_str}_fold_{sub_split_ctr}.csv')
+                valid_loss_step = np.loadtxt(f'{results_dir}/stats/valid_loss_v1_class_{num_classes}_subject_{sub_str}_fold_{sub_split_ctr}.csv')
+           
+                train_accu += train_accu_step
+                valid_accu += valid_accu_step
+                train_loss += train_loss_step
+                valid_loss += valid_loss_step
+            
+            train_accu = train_accu/num_splits
+            valid_accu = valid_accu/num_splits
+            train_loss = train_loss/num_splits
+            valid_loss = valid_loss/num_splits
 
-    np.savetxt(f'{results_dir}/stats/train_accu_v1_avg.csv', train_accu)
-    np.savetxt(f'{results_dir}/stats/valid_accu_v1_avg.csv', valid_accu)
-    np.savetxt(f'{results_dir}/stats/train_loss_v1_avg.csv', train_loss)
-    np.savetxt(f'{results_dir}/stats/valid_loss_v1_avg.csv', valid_loss)
+            np.savetxt(f'{results_dir}/stats/train_accu_v1_class_{num_classes}_subject_{sub_str}_avg.csv', train_accu)
+            np.savetxt(f'{results_dir}/stats/valid_accu_v1_class_{num_classes}_subject_{sub_str}_avg.csv', valid_accu)
+            np.savetxt(f'{results_dir}/stats/train_loss_v1_class_{num_classes}_subject_{sub_str}_avg.csv', train_loss)
+            np.savetxt(f'{results_dir}/stats/valid_loss_v1_class_{num_classes}_subject_{sub_str}_avg.csv', valid_loss)
 
-    # Plot Accuracy 
-    plt.plot(train_accu, label='Training')
-    plt.plot(valid_accu, label='Validation')
-    plt.title(f'Accuracy: LR: 20-30-50, DR=0.2')
-    plt.xlabel('Epochs')
-    plt.ylabel('Accuracy')
-    plt.legend()
-    plt.savefig(f'{results_dir}/plots/accu_global_avg.pdf')
-    plt.clf()
-    # Plot Loss
-    plt.plot(train_loss, label='Training')
-    plt.plot(valid_loss, label='Validation')
-    plt.title(f'Loss: LR: 20-30-50, DR=0.2')
-    plt.xlabel('Epochs')
-    plt.ylabel('Loss')
-    plt.legend()
-    plt.savefig(f'{results_dir}/plots/loss_global_avg.pdf')
-    plt.clf()
+            # Plot Accuracy 
+            plt.plot(train_accu, label='Training')
+            plt.plot(valid_accu, label='Validation')
+            plt.title(f'S:{sub_str} Acc.: LR: 2-3-5, DR=0.2')
+            plt.xlabel('Epochs')
+            plt.ylabel('Accuracy')
+            plt.legend()
+            plt.savefig(f'{results_dir}/plots/accu_global_avg.pdf')
+            plt.clf()
+            # Plot Loss
+            plt.plot(train_loss, label='Training')
+            plt.plot(valid_loss, label='Validation')
+            plt.title(f'S:{sub_str} Loss: LR: 2-3-5, DR=0.2')
+            plt.xlabel('Epochs')
+            plt.ylabel('Loss')
+            plt.legend()
+            plt.savefig(f'{results_dir}/plots/loss_global_avg.pdf')
+            plt.clf()
 
-
+        split_ctr = split_ctr + 1
